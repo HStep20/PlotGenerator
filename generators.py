e8a4fd701b2383af386726c827ea6207e21b64fa
HStep20 <Stephens.j.hunter@protonmail.com>
2020-09-25 22:42:00 -0600

    ADD: Variable amounts of cultural impact for story influence.

--- generators.py
+++ generators.py
@@ -1,11 +1,13 @@
 import random
+
 Genre = []
 Plot = []
 WBCultural = []
 WBPhysical = []
 Theme = []
-datasets = [Genre, Plot, WBCultural, WBPhysical, Theme]
+datasets = [Genre, Plot, Theme]
 output = []
+WBAspects = ""
 
 
 def setup():
@@ -26,19 +28,35 @@ def generate():
     output.clear()
     for item in datasets:
         output.append(item[random.randint(0, len(item) - 1)])
+    WBAspects = designateWorldbuilding()
     return returnOutput()
 
+
+def designateWorldbuilding():
+    WBString = ""
+    count = random.randint(1, 4)
+    for i in range(count):
+        culturalOrPhysical = random.randint(0, 1)
+        if culturalOrPhysical == 0:
+            WBString += WBCultural[random.randint(0, len(WBCultural) - 1)]
+        else:
+            WBString += WBPhysical[random.randint(0, len(WBPhysical) - 1)]
+        if (i == (count - 2)) and (count != 1):
+            WBString += ", and "
+        elif not (i == count - 1):
+            WBString += ", "
+    print(WBString)
+    return WBString
 
 def returnOutput():
     if output[0][0] in ('A', 'E', 'I', 'O', 'U'):
         article = "an "
     else:
         article = "a "
-    
-    return(
-        "Brainstorm " + article + output[0] + " book "
-        "using the " + output[1] + " plot "
-        "where " + output[2] + " and " +
-        output[3] + " play a role in the story, "
-        "and it touches on the theme of " + output[4]
+
+    return (
+            "Brainstorm " + article + output[0] + " book "
+            "using the " + output[1] + " plot "
+            "where " + WBAspects + " play a role in the story, "
+            "and it touches on the theme of " + output[2]
     )
