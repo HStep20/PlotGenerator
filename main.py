7cf2270ca860ffce60229c1ec717dfbcb0ec7791
HStep20 <Stephens.j.hunter@protonmail.com>
2020-09-25 21:59:57 -0600

    ADD: Reddit Bot functionality

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from generators import generate, setup
-import tweepy
+import tweepy, praw
 from tokens import *
 
-auth = tweepy.OAuthHandler(keys2["consumerKey"], keys2["consumerSecretKey"])
-auth.set_access_token(keys2["accessToken"], keys2["accessTokenSecret"])
-
+#Twitter
+auth = tweepy.OAuthHandler(twitterKeys2["consumerKey"], twitterKeys2["consumerSecretKey"])
+auth.set_access_token(twitterKeys2["accessToken"], twitterKeys2["accessTokenSecret"])
 api = tweepy.API(auth)
-
 setup()
 prompt = generate()
 api.update_status(prompt)
+
+#Reddit
+reddit = praw.Reddit(
+    client_id=redditKeys["client_id"],
+    client_secret=redditKeys["client_secret"],
+    user_agent=redditKeys["user_agent"],
+    username="BrainstormCentral",
+    password=redditKeys["password"]
+)
+reddit.subreddit("BrainstormPrompts").submit(prompt, selftext="")
