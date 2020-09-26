472657386fddb29c7955aca8c94931a541d579ae
HStep20 <Stephens.j.hunter@protonmail.com>
2020-09-25 20:49:13 -0600

    ADD: Twitter Functionality: https://twitter.com/BrainstormCent1

--- generators.py
+++ generators.py
@@ -7,6 +7,7 @@ Theme = []
 datasets = [Genre, Plot, WBCultural, WBPhysical, Theme]
 output = []
 
+
 def setup():
     for line in open('GenFiles/Genre.txt', 'r'):
         Genre.append(line.rstrip())
@@ -19,15 +20,25 @@ def setup():
     for line in open('GenFiles/Theme.txt', 'r'):
         Theme.append(line.rstrip())
     generate()
+
+
 def generate():
     output.clear()
     for item in datasets:
         output.append(item[random.randint(0, len(item) - 1)])
     return returnOutput()
+
+
 def returnOutput():
+    if output[0][0] in ('A', 'E', 'I', 'O', 'U'):
+        article = "an "
+    else:
+        article = "a "
+    
     return(
-        "Brainstorm a " + output[0] + " book "
-        "using the " + output[1] + " plot " 
-        "where " + output[2] + " and " + output[3] + " play a role in the story, "
+        "Brainstorm " + article + output[0] + " book "
+        "using the " + output[1] + " plot "
+        "where " + output[2] + " and " +
+        output[3] + " play a role in the story, "
         "and it touches on the theme of " + output[4]
-          )
+    )
--- main.py
+++ main.py
@@ -1,47 +1,12 @@
 from generators import generate, setup
-import tkinter as tk
-import tkinter.font as tkFont
+import tweepy
+from tokens import *
 
-class App:
+auth = tweepy.OAuthHandler(keys2["consumerKey"], keys2["consumerSecretKey"])
+auth.set_access_token(keys2["accessToken"], keys2["accessTokenSecret"])
 
-    def __init__(self, root):
-        #setting title
-        root.title("A Simple Plot Generator")
-        #setting window size
-        width=600
-        height=500
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.configure(background='#22577a')
-        root.resizable(width=False, height=False)
+api = tweepy.API(auth)
 
-        self.GLabel_544=tk.Label(root)
-        self.GLabel_544["bg"] = "#80ed99"
-        ft = tkFont.Font(family='Times',size=26)
-        self.GLabel_544["font"] = ft
-        self.GLabel_544["fg"] = "#22577a"
-        self.GLabel_544["justify"] = "center"
-        self.GLabel_544["wraplength"] = 512
-        self.GLabel_544["text"] = ""
-        self.GLabel_544.place(x=20,y=20,width=550,height=300)
-
-        GButton_483=tk.Button(root)
-        GButton_483["bg"] = "#38a3a5"
-        ft = tkFont.Font(family='Times',size=30)
-        GButton_483["font"] = ft
-        GButton_483["fg"] = "#22577a"
-        GButton_483["justify"] = "center"
-        GButton_483["text"] = "Generate Story"
-        GButton_483.place(x=70,y=350,width=460,height=87)
-        GButton_483["command"] = self.GButton_483_command
-
-    def GButton_483_command(self):
-        self.GLabel_544["text"] = generate()
-
-if __name__ == "__main__":
-    setup()
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+setup()
+prompt = generate()
+api.update_status(prompt)
